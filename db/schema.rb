e6ea5a1d0ec7700b32782841f40edb30ec05a2b8
lttviet <lttviet@gmail.com>
2015-03-02 15:16:43 -0500

    create account model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302201135) do
+ActiveRecord::Schema.define(version: 20150302201443) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "attendances", force: :cascade do |t|
     t.integer  "event_id"
