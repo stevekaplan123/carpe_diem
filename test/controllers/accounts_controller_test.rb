94baebc826c17c02701e573b97540b60e7c23330
lttviet <lttviet@gmail.com>
2015-03-02 15:34:15 -0500

    add tests for accounts

---
+++ test/controllers/accounts_controller_test.rb
@@ -0,0 +1,14 @@
+test "should create account" do
+  assert_difference('Account.count') do
+    post :create, account: { name: 'sam', password: 'secret',
+      password_confirmation: 'secret' }
+  end
+
+  assert_redirected_to accounts_path
+end
+
+test "should update account" do
+  patch :update, id: @account, account: { name: @account.name,
+    password: 'secret', password_confirmation: 'secret' }
+  assert_redirected_to accounts_path
+end
