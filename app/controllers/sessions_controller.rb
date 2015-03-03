cc6559f6c43abdc9126646043339c4d07a75249d
lttviet <lttviet@gmail.com>
2015-03-02 20:03:10 -0500

    modify sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    account = Account.find_by(name: params[:name])
+    if account and account.authenticate(params[:password])
+      session[:account_id] = account.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Invalid user/password"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_url, notice: "Logged out"
   end
 end
