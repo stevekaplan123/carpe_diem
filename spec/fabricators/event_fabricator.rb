c19293dd05bef1a7499671a57da7487cb2d2c02e
lttviet <lttviet@gmail.com>
2015-03-02 19:09:28 -0500

    remove time_creation from favricator

--- spec/fabricators/event_fabricator.rb
+++ spec/fabricators/event_fabricator.rb
@@ -1,7 +1,6 @@
 Fabricator(:event) do
   creator_id { Faker::Number.number(2) }
   event_name { Faker::Lorem.words }
-  time_creation { Faker::Time.forward(1, :morning) }
   time_occurrence { Faker::Time.forward(23, :evening) }
   location { Faker::Address.street_address }
   longitude { Faker::Address.longitude }
