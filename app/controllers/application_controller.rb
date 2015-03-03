8c46d9a0f5cdc220e653ad3892b6bce2ef103d83
Ralph <ralphxiaoz@gmail.com>
2015-03-03 16:49:13 -0500

    implemented logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  # before_action :authorize
+  # check session on every page. will have to rewrite the tests
+  before_action :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
