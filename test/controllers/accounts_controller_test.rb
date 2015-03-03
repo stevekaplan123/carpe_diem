b3eeefe3ad579d20861ce5662d5a0333fcfb12ab
lttviet <lttviet@gmail.com>
2015-03-02 20:48:08 -0500

    fix tests

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
 
 class AccountsControllerTest < ActionController::TestCase
+  setup do
+    @account = accounts(:one)
+  end
+
   test "should create account" do
     assert_difference('Account.count') do
-      post :create, account: { name: 'sfsaem', password: 'secret', password_confirmation: 'secret' }
+      post :create, account: { name: Faker::Internet.user_name,
+        password: 'secret', password_confirmation: 'secret' }
     end
     assert_redirected_to accounts_path
   end
 
-  #test "should update account" do
-  #  patch :update, id: @account, account: { name: @account.name,
-  #    password: 'secret', password_confirmation: 'secret' }
-  #  assert_redirected_to accounts_path
-  #end
+  test "should update account" do
+    patch :update, id: @account, account: { name: @account.name,
+      password: 'secret', password_confirmation: 'secret' }
+    assert_redirected_to accounts_path
+  end
 end
