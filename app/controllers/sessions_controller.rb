8395c4b2ae33a9cf398ff81edb3f845d14ac8613
Ralph <ralphxiaoz@gmail.com>
2015-03-05 14:59:38 -0500

    Revert "authentication following the turorial"
    This reverts commit 63f356a9d771c01e808a18110bebda4bae16f3d8.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,20 @@
 class SessionsController < ApplicationController
-
+  skip_before_action :authorize
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+    user = User.find_by(name: params[:name])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
     else
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
-      render 'new'
+      redirect_to login_url, alert: "Invalid user/password"
     end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out"
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,2 @@
 module SessionsHelper
-
-  # Logs in the given user.
-  def log_in(user)
-    session[:user_id] = user.id
-  end
 end
