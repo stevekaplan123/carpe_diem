bebe047ac9ebb024e8a4a7c996b04750f0006ae9
lttviet <lttviet@gmail.com>
2015-03-11 04:33:17 -0400

    add admin column to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310154817) do
+ActiveRecord::Schema.define(version: 20150311061013) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer  "event_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150310154817) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.boolean  "admin"
   end
 
 end
