b0f0db8c2602ba7e4dafbad696baef5459197f65
lttviet <lttviet@gmail.com>
2015-03-17 16:30:50 -0400

    user name is downcased before adding to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_many :attendances
   has_many :events, through: :attendances
-  before_save {self.email = email.downcase}
+  before_save { self.email = email.downcase,
+                self.name = name.downcase }
   validates :name, presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@brandeis.edu/i
