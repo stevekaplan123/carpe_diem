92d3529e1dda1ce445f5e834e3f5a44a0804d463
Vladimir Susaya <vladimir.susaya@gmail.com>
2015-03-20 19:59:05 -0400

    update db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20150318192846) do
 
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
   create_table "attendances", force: :cascade do |t|
     t.integer  "event_id"
     t.integer  "user_id"
@@ -30,7 +37,6 @@ ActiveRecord::Schema.define(version: 20150318192846) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.integer  "user_id"
     t.integer  "creator_id"
     t.string   "name"
     t.datetime "time_occurrence"
@@ -52,10 +58,10 @@ ActiveRecord::Schema.define(version: 20150318192846) do
     t.string   "name"
     t.string   "email"
     t.integer  "num_events"
-    t.boolean  "admin"
-    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.boolean  "admin"
   end
 
 end
